922215cb1002e61229c5e1d8ef4fdf7bad0cee90
Victor Hugo Ramos <victorhramos.ti@outlook.com>
2026-06-08 14:56:21 -0300

    feat(engine): finalize distributed workflow engine

---
+++ nexus_os/core/runtime/dispatcher.py
@@ -0,0 +1,5 @@
+from nexus_os.core.runtime.simple_queue import task_queue
+
+
+def dispatch_task(task_data: dict):
+    task_queue.put(task_data)
---
+++ nexus_os/core/runtime/simple_queue.py
@@ -0,0 +1,3 @@
+import queue
+
+task_queue = queue.Queue()
